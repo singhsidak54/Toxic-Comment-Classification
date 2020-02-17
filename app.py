d151f7dfc067e4143d1d98376c76719b8d1e526e
Sidakdeep Singh <singhsidak54@gmail.com>
2020-02-17 12:31:25 +0530

    minor fix

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import pickle
-from tcc_model import MEModelNB
-from flask import Flask, request, jsonify
+from model.tcc_model import MEModelNB
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-model = open('model.pkl', 'rb')
 
-clf = pickle.load(model)
+@app.route('/')
+def predict_form():
+    return render_template('index.html')
 
-@app.route("/predict", methods=["POST"])
+@app.route("/", methods=["POST"])
 def predict():
 
-    comment = request.json['comment']
+    comment = request.form['comment']
 
     pred = clf.predict(comment)
 
     return jsonify({'class': pred})
 
 if __name__ == '__main__':
+    model = open('model/model.pkl', 'rb')
 
+    clf = pickle.load(model)
     app.run(debug=True)
